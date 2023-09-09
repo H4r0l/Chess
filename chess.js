d2833d511dec75835878b3eb46cc8e792b47f734
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-09-09 17:01:15 -0500

    fix: appearance and pieces test

--- chess.js
+++ chess.js
@@ -1,51 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const canva = document.getElementById("canvas");
-  var ctx = canva.getContext("2d");
-  
-  const svgking = "./assets/black_king.svg"
-  const svgqueen = "./assets/black_queen.svg"
-  const svgbishop = "./assets/black_bishop.svg"
-  const svgknight = "./assets/black_knight.svg"
-  const svgrook = "./assets/black_rook.svg"
-  const svgpawn = "./assets/black_pawn.svg"
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-    var isGameOver = false
-    let whiteTurn = false
-    let blackTurn = false
+  const svgPaths = {
+    king: "./assets/black_king.svg",
+    queen: "./assets/black_queen.svg",
+    bishop: "./assets/black_bishop.svg",
+    knight: "./assets/black_knight.svg",
+    rook: "./assets/black_rook.svg",
+    pawn: "./assets/black_pawn.svg",
+  };
 
-    var chessboard = [
-      [0, 1, 0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0, 1, 0],
-    ];
+  const chessboard = [
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+  ];
 
-    const drawChess = () => {
-      let rows = chessboard.length;
-      let columns = chessboard[0].length;
-      for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < columns; col++) {
-          if (chessboard[row][col] == 1) {
-            ctx.fillStyle = "black";
-            ctx.fillRect(col * 50, row * 50, 50, 50);
-          }
-          if (chessboard[row] == 4 && chessboard[col] == 8) {
-            
-          }
+  const loadImage = (path) => {
+    const img = new Image();
+    img.src = path;
+    return img;
+  };
 
+  const king = loadImage(svgPaths.king);
+
+  king.onload = () => {
+    drawChess();
+  };
+
+  const drawChess = () => {
+    let rows = chessboard.length;
+    let columns = chessboard[0].length;
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < columns; col++) {
+        if (chessboard[row][col] == 1) {
+          ctx.fillStyle = "#009D71";
+          ctx.fillRect(col * 50, row * 50, 50, 50);
         }
       }
-
-    };
-  //Mientras que la partida no haya terminado 
-  while (isGameOver == false) {
-    drawChess();
-    if (whiteTurn) {
-      
     }
-  }
-})
+
+    // Draw the king in the middle square
+    const kingRow = 4;
+    const kingCol = 7;
+
+    const x = kingCol * 50;
+    const y = kingRow * 50;
+
+    ctx.drawImage(king, x, y, 50, 50);
+  };
+});
