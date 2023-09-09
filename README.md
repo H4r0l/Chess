eee872c1f965a804f85042d97d64da11c8555619
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-09-08 22:30:27 -0500

    Readme update & Assets

---
+++ README.md
@@ -0,0 +1,23 @@
+# Chess Game Repo
+
+This repository contains a simple implementation of a chess game using HTML, CSS, and JavaScript. The game provides a basic interface for playing chess against another person on a single device.
+
+## Features
+
+- Two-player gameplay on the same device.
+- Standard chess rules and movements.
+- Interactive chessboard with piece highlighting.
+
+## Getting Started
+
+To play the game, simply open the `index.html` file in a web browser. The game will load, and you can start playing immediately.
+
+## How to Play
+
+1. Each player takes turns to move their pieces.
+2. Click on a piece to select it, then click on a highlighted square to move the piece.
+3. The game enforces standard chess rules, including check and checkmate conditions.
+
+## Customization
+
+The game's appearance and behavior can be customized by modifying the CSS and JavaScript files. You can adjust colors, styles, or even add additional features to enhance the game.
--- chess.js
+++ chess.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const canva = document.getElementById("canvas");
-    var ctx = canva.getContext("2d");
+  var ctx = canva.getContext("2d");
+  
+  const svgking = "./assets/black_king.svg"
+  const svgqueen = "./assets/black_queen.svg"
+  const svgbishop = "./assets/black_bishop.svg"
+  const svgknight = "./assets/black_knight.svg"
+  const svgrook = "./assets/black_rook.svg"
+  const svgpawn = "./assets/black_pawn.svg"
+
+    var isGameOver = false
+    let whiteTurn = false
+    let blackTurn = false
 
     var chessboard = [
       [0, 1, 0, 1, 0, 1, 0, 1],
@@ -16,15 +27,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const drawChess = () => {
       let rows = chessboard.length;
       let columns = chessboard[0].length;
-      for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-          if (chessboard[i][j] == 1) {
+      for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < columns; col++) {
+          if (chessboard[row][col] == 1) {
             ctx.fillStyle = "black";
-            ctx.fillRect(j * 50, i * 50, 50, 50);
+            ctx.fillRect(col * 50, row * 50, 50, 50);
           }
+          if (chessboard[row] == 4 && chessboard[col] == 8) {
+            
+          }
+
         }
       }
+
     };
+  //Mientras que la partida no haya terminado 
+  while (isGameOver == false) {
     drawChess();
-    
+    if (whiteTurn) {
+      
+    }
+  }
 })
